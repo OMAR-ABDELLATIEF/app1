2a8d0b283be4b64e0570d1b1e95477c253dfa911
OMAR-ABDELLATIEF <omar.abdellatief2@gmail.com>
2024-11-24 18:56:19 +0200

    Final

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -8,8 +8,9 @@ export default function Card() {
             <img src="https://img.freepik.com/free-photo/aerial-drone-view-zadar-sunset-croatia-historical-city-centre-with-old-buildings_1268-23572.jpg?t=st=1732466395~exp=1732469995~hmac=73bd4487a66584468a06fdddaeba139f2427484e2e457f94ef6b1be7be888165&w=826" alt="Buildings" />
         </div>
         <div className="card-content">
-        <h1>San Francisco</h1>
-        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ex fugiat voluptatum inventore consectetur maiores sapiente ipsa veritatis nobis quod officia?</p>
+            <h1>San Francisco</h1>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ex fugiat voluptatum inventore consectetur maiores sapiente ipsa veritatis nobis quod officia?</p>
+            <button>details</button>
         </div>
     </div>
   )
